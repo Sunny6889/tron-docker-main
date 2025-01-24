f0aaeb97031750387c59d7fbfcec2b36c3fefa14
Sunny Jiao <>
2025-01-24 13:17:13 +0800

    fix shell

--- tools/docker/test.sh
+++ tools/docker/test.sh
@@ -20,6 +20,7 @@ export GOSS_PATH=$TEST_PATH/goss-linux-amd64
 export GOSS_OPTS="$GOSS_OPTS --format junit"
 export GOSS_FILES_STRATEGY=cp
 DOCKER_IMAGE=$1
+# shellcheck disable=SC2034
 DOCKER_FILE="${2:-$PWD/Dockerfile}"
 
 i=0
@@ -28,8 +29,11 @@ i=0
 # we test that things listen on the right interface/port, not what interface the advertise
 # hence we dont set p2p-host=0.0.0.0 because this sets what its advertising to devp2p; the important piece is that it defaults to listening on all interfaces
 GOSS_FILES_PATH=$TEST_PATH/01 \
-bash $TEST_PATH/dgoss run --sysctl net.ipv6.conf.all.disable_ipv6=1 $DOCKER_IMAGE \
-#-p 8090:8090 -p 8091:8091 -p 18888:18888 -p 18888:18888/udp -p 50051:50051 \
+bash $TEST_PATH/dgoss run --sysctl net.ipv6.conf.all.disable_ipv6=1 "$DOCKER_IMAGE" \
+#-p 8090:8090 -p 8091:8091 -p 18888:18888 -p 18888:18888/udp -p 50051:50051
+# shellcheck disable=SC2006
+# shellcheck disable=SC2003
+# shellcheck disable=SC2188
 > ./reports/01.xml || i=`expr $i + 1`
 
-exit $i
+exit "$i"
